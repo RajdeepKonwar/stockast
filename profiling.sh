d57e39b503e37236b6a1af8e9d68ebbd33218fef
Rajdeep Konwar <rajdeep.konwr@gmail.com>
2022-01-17 12:33:11 -0500

    Update license year to 2022

--- profiling.sh
+++ profiling.sh
@@ -5,7 +5,7 @@
 # @section LICENSE
 # MIT License
 # 
-# Copyright (c) 2017-2021 Rajdeep Konwar
+# Copyright (c) 2017-2022 Rajdeep Konwar
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
