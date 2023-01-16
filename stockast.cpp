9940256b35c8d08bb5d7c76b91438f449148d121
Rajdeep Konwar <rajdeep.konwr@gmail.com>
2023-01-16 00:52:38 -0500

    Switch style to camelCasing

--- stockast.cpp
+++ stockast.cpp
@@ -46,13 +46,12 @@
 //----------------------------------------------------------------------------
 // Calculates volatility from data.csv file
 //----------------------------------------------------------------------------
-float calculate_volatility(float spot_price, int time_steps)
+float calculateVolatility(float spotPrice, int timeSteps)
 {
     // Open data.csv in read-mode, exit on fail
-    const char* file_name = "data.csv";
-    std::ifstream file_ptr;
-    file_ptr.open(file_name, std::ifstream::in);
-    if (!file_ptr.is_open())
+    std::ifstream filePtr;
+    filePtr.open("data.csv", std::ifstream::in);
+    if (!filePtr.is_open())
     {
         std::cerr << "Cannot open data.csv! Exiting..\n";
         exit(EXIT_FAILURE);
@@ -60,16 +59,16 @@ float calculate_volatility(float spot_price, int time_steps)
 
     std::string line;
     // Read the first line then close file
-    if (!std::getline(file_ptr, line))
+    if (!std::getline(filePtr, line))
     {
         std::cerr << "Cannot read from data.csv! Exiting..\n";
-        file_ptr.close();
+        filePtr.close();
         exit(EXIT_FAILURE);
     }
-    file_ptr.close();
+    filePtr.close();
 
-    int i = 0, len = time_steps - 1;
-    std::unique_ptr<float[]> priceArr = std::make_unique<float[]>(time_steps - 1);
+    int i = 0, len = timeSteps - 1;
+    std::unique_ptr<float[]> priceArr = std::make_unique<float[]>(timeSteps - 1);
     std::istringstream iss(line);
     std::string token;
 
@@ -77,47 +76,47 @@ float calculate_volatility(float spot_price, int time_steps)
     while (std::getline(iss, token, ','))
         priceArr[i++] = std::stof(token);
 
-    float sum = spot_price;
+    float sum = spotPrice;
     // Find mean of the estimated minute-end prices
     for (i = 0; i < len; i++)
         sum += priceArr[i];
-    float mean_price = sum / (len + 1);
+    float meanPrice = sum / (len + 1);
 
     // Calculate market volatility as standard deviation
-    sum = powf((spot_price - mean_price), 2.0f);
+    sum = powf((spotPrice - meanPrice), 2.0f);
     for (i = 0; i < len; i++)
-        sum += powf((priceArr[i] - mean_price), 2.0f);
+        sum += powf((priceArr[i] - meanPrice), 2.0f);
 
-    float std_dev = sqrtf(sum);
+    float stdDev = sqrtf(sum);
 
     // Return as percentage
-    return std_dev / 100.0f;
+    return stdDev / 100.0f;
 }
 
 /** ---------------------------------------------------------------------------
-    Finds mean of a 2D array across first index (in_loops)
-    M is in/out_loops and N is time_steps
+    Finds mean of a 2D array across first index (inLoops)
+    M is in/outLoops and N is timeSteps
 ----------------------------------------------------------------------------*/
-float* find_2d_mean(float** matrix, int num_loops, int time_steps)
+float* find2dMean(float** matrix, int numLoops, int timeSteps)
 {
     int j;
-    float* avg = new float[time_steps];
+    float* avg = new float[timeSteps];
     float sum = 0.0f;
 
-    for (int i = 0; i < time_steps; i++)
+    for (int i = 0; i < timeSteps; i++)
     {
         /** A private copy of 'sum' variable is created for each thread.
             At the end of the reduction, the reduction variable is applied to
             all private copies of the shared variable, and the final result
             is written to the global shared variable. **/
 #pragma omp parallel for private(j) reduction(+:sum)
-        for (j = 0; j < num_loops; j++)
+        for (j = 0; j < numLoops; j++)
         {
             sum += matrix[j][i];
         }
 
         // Calculating average across columns
-        avg[i] = sum / num_loops;
+        avg[i] = sum / numLoops;
         sum = 0.0f;
     }
 
@@ -128,34 +127,34 @@ float* find_2d_mean(float** matrix, int num_loops, int time_steps)
     Generates a random number seeded by system clock based on standard
     normal distribution on taking mean 0.0 and standard deviation 1.0
 ----------------------------------------------------------------------------*/
-float rand_gen(float mean, float std_dev)
+float genRand(float mean, float stdDev)
 {
     auto seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(static_cast<unsigned int>(seed));
-    std::normal_distribution<float> distribution(mean, std_dev);
+    std::normal_distribution<float> distribution(mean, stdDev);
     return distribution(generator);
 }
 
 //----------------------------------------------------------------------------
 // Simulates Black Scholes model
 //----------------------------------------------------------------------------
-float* run_black_scholes_model(float spot_price, int time_steps, float risk_rate, float volatility)
+float* runBlackScholesModel(float spotPrice, int timeSteps, float riskRate, float volatility)
 {
-    float  mean = 0.0f, std_dev = 1.0f;                                             // Mean and standard deviation
-    float  deltaT = 1.0f / time_steps;                                              // Timestep
-    std::unique_ptr<float[]> norm_rand = std::make_unique<float[]>(time_steps - 1); // Array of normally distributed random nos.
-    float* stock_price = new float[time_steps];                                     // Array of stock price at diff. times
-    stock_price[0] = spot_price;                                                    // Stock price at t=0 is spot price
+    float  mean = 0.0f, stdDev = 1.0f;                                             // Mean and standard deviation
+    float  deltaT = 1.0f / timeSteps;                                              // Timestep
+    std::unique_ptr<float[]> normRand = std::make_unique<float[]>(timeSteps - 1); // Array of normally distributed random nos.
+    float* stockPrice = new float[timeSteps];                                     // Array of stock price at diff. times
+    stockPrice[0] = spotPrice;                                                    // Stock price at t=0 is spot price
 
     // Populate array with random nos.
-    for (int i = 0; i < time_steps - 1; i++)
-        norm_rand[i] = rand_gen(mean, std_dev);
+    for (int i = 0; i < timeSteps - 1; i++)
+        normRand[i] = genRand(mean, stdDev);
 
     // Apply Black Scholes equation to calculate stock price at next timestep
-    for (int i = 0; i < time_steps - 1; i++)
-        stock_price[i + 1] = stock_price[i] * exp(((risk_rate - (powf(volatility, 2.0f) / 2.0f)) * deltaT) + (volatility * norm_rand[i] * sqrtf(deltaT)));
+    for (int i = 0; i < timeSteps - 1; i++)
+        stockPrice[i + 1] = stockPrice[i] * exp(((riskRate - (powf(volatility, 2.0f) / 2.0f)) * deltaT) + (volatility * normRand[i] * sqrtf(deltaT)));
 
-    return stock_price;
+    return stockPrice;
 }
 
 //----------------------------------------------------------------------------
@@ -165,28 +164,28 @@ int main(int argc, char** argv)
 {
     const auto beginTime = std::chrono::system_clock::now();
 
-    int in_loops = 100;     // Inner loop iterations
-    int out_loops = 10000;  // Outer loop iterations
-    int time_steps = 180;   // Stock market time-intervals (min)
+    int inLoops = 100;     // Inner loop iterations
+    int outLoops = 10000;  // Outer loop iterations
+    int timeSteps = 180;   // Stock market time-intervals (min)
 
     // Matrix for stock-price vectors per iteration
-    float** stock = new float* [in_loops];
-    for (int i = 0; i < in_loops; i++)
-        stock[i] = new float[time_steps];
+    float** stock = new float* [inLoops];
+    for (int i = 0; i < inLoops; i++)
+        stock[i] = new float[timeSteps];
 
     // Matrix for mean of stock-price vectors per iteration
-    float** avg_stock = new float* [out_loops];
-    for (int i = 0; i < out_loops; i++)
-        avg_stock[i] = new float[time_steps];
+    float** avgStock = new float* [outLoops];
+    for (int i = 0; i < outLoops; i++)
+        avgStock[i] = new float[timeSteps];
 
     // Vector for most likely outcome stock price
-    float* opt_stock = new float[time_steps];
+    float* optStock = new float[timeSteps];
 
-    float risk_rate = 0.001f;   // Risk free interest rate (%)
-    float spot_price = 100.0f;  // Spot price (at t = 0)
+    float riskRate = 0.001f;   // Risk free interest rate (%)
+    float spotPrice = 100.0f;  // Spot price (at t = 0)
 
     // Market volatility (calculated from data.csv)
-    float volatility = calculate_volatility(spot_price, time_steps);
+    float volatility = calculateVolatility(spotPrice, timeSteps);
 
     // Welcome message
     std::cout << "--Welcome to Stockast: Stock Forecasting Tool--\n";
@@ -210,44 +209,44 @@ int main(int argc, char** argv)
             grabs "chunk" iterations until all iterations are done.
             Faster threads are assigned more iterations (not Round Robin) **/
 #pragma omp for schedule(dynamic)
-        for (i = 0; i < out_loops; i++)
+        for (i = 0; i < outLoops; i++)
         {
             /** Using Black Scholes model to get stock price every iteration
-                Returns data as a column vector having rows=time_steps  **/
-            for (int j = 0; j < in_loops; j++)
-                stock[j] = run_black_scholes_model(spot_price, time_steps, risk_rate, volatility);
+                Returns data as a column vector having rows=timeSteps  **/
+            for (int j = 0; j < inLoops; j++)
+                stock[j] = runBlackScholesModel(spotPrice, timeSteps, riskRate, volatility);
 
             // Stores average of all estimated stock-price arrays
-            avg_stock[i] = find_2d_mean(stock, in_loops, time_steps);
+            avgStock[i] = find2dMean(stock, inLoops, timeSteps);
         }
         //---> Implicit omp barrier <--
     }
 
     // Average of all the average arrays
-    opt_stock = find_2d_mean(avg_stock, out_loops, time_steps);
+    optStock = find2dMean(avgStock, outLoops, timeSteps);
 
     // Write optimal outcome to disk
-    std::ofstream file_ptr;
-    file_ptr.open("opt.csv", std::ofstream::out);
-    if (!file_ptr.is_open())
+    std::ofstream filePtr;
+    filePtr.open("opt.csv", std::ofstream::out);
+    if (!filePtr.is_open())
     {
         std::cerr << "Couldn't open opt.csv! Exiting..\n";
         return EXIT_FAILURE;
     }
 
-    for (i = 0; i < time_steps; i++)
-        file_ptr << opt_stock[i] << "\n";
-    file_ptr.close();
+    for (i = 0; i < timeSteps; i++)
+        filePtr << optStock[i] << "\n";
+    filePtr.close();
 
-    for (i = 0; i < in_loops; i++)
+    for (i = 0; i < inLoops; i++)
         delete[] stock[i];
     delete[] stock;
 
-    for (i = 0; i < out_loops; i++)
-        delete[] avg_stock[i];
-    delete[] avg_stock;
+    for (i = 0; i < outLoops; i++)
+        delete[] avgStock[i];
+    delete[] avgStock;
 
-    delete[] opt_stock;
+    delete[] optStock;
 
     std::cout << " done!\n  Time taken = " << std::to_string(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - beginTime).count()) << "s";
     return getchar();
